359c2060742acbd73f92755e061c5006541c658f
J27mendez <mendes_j27@hotmail.com>
2025-03-14 15:52:07 -0300

    feat: trying to overwrite the database

--- api/server.js
+++ api/server.js
@@ -1,16 +1,41 @@
 const jsonServer = require("json-server");
 const path = require("path");
+const fs = require("fs");
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
+const filePath = path.join(__dirname, "db.json");
 
-server.use(middlewares);
+const readDbFile = () => {
+  const data = fs.readFileSync(filePath, "utf-8");
+  return JSON.parse(data);
+};
+
+const saveDbFile = (db) => {
+  fs.writeFileSync(filePath, JSON.stringify(db, null, 2), "utf-8");
+};
+
+server.use((req, res, next) => {
+  req.db = readDbFile();
+  next();
+});
+
+const router = jsonServer.router(readDbFile());
+
+server.use(jsonServer.defaults());
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
+
+server.use((req, res, next) => {
+  res.on("finish", () => {
+    if (["POST", "PUT", "DELETE"].includes(req.method)) {
+      saveDbFile(router.db.getState());
+    }
+  });
+  next();
+});
 
 server.use(router);
 
