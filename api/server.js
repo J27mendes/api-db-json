de15bf76e938e641b682f7ef780acebc39b8f30c
J27mendez <mendes_j27@hotmail.com>
2025-03-14 15:56:33 -0300

    refactor: return to origin server

--- api/server.js
+++ api/server.js
@@ -3,44 +3,23 @@ const path = require("path");
 const fs = require("fs");
 
 const server = jsonServer.create();
+
 const filePath = path.join(__dirname, "db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults();
 
-const readDbFile = () => {
-  const data = fs.readFileSync(filePath, "utf-8");
-  return JSON.parse(data);
-};
-
-const saveDbFile = (db) => {
-  fs.writeFileSync(filePath, JSON.stringify(db, null, 2), "utf-8");
-};
-
-server.use((req, res, next) => {
-  req.db = readDbFile();
-  next();
-});
-
-const router = jsonServer.router(readDbFile());
-
-server.use(jsonServer.defaults());
+server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
 
-server.use((req, res, next) => {
-  res.on("finish", () => {
-    if (["POST", "PUT", "DELETE"].includes(req.method)) {
-      saveDbFile(router.db.getState());
-    }
-  });
-  next();
-});
-
 server.use(router);
-
 server.listen(3000, () => {
-  console.log("JSON Server rodando na porta 3000 🚀");
+  console.log("JSON Server");
 });
 
 module.exports = server;
